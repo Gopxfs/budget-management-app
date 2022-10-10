3eb821d3a09d96557696951ad58adf5b61c13ff3
Gabriel <gopxfs@hotmail.com>
2022-10-10 17:11:08 -0300

    Add relationship between models

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,2 +1,4 @@
 class Budget < ApplicationRecord
+  has_and_belongs_to_many :groups
+  belongs_to :author, class_name: 'User', foreign_key: :user_id
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ApplicationRecord
+  has_and_belongs_to_many :budgets
+  belongs_to :user
 end
