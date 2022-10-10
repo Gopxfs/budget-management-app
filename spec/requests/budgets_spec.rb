2dd31a570bd08118e9343be8c853a0f8b68bac37
Gabriel <gopxfs@hotmail.com>
2022-10-10 17:46:28 -0300

    Fix linter errors

--- spec/requests/budgets_spec.rb
+++ spec/requests/budgets_spec.rb
@@ -12,69 +12,68 @@ require 'rails_helper'
 # of tools you can use to make these specs even more expressive, but we're
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
-RSpec.describe "/budgets", type: :request do
-  
+RSpec.describe '/budgets', type: :request do
   # This should return the minimal set of attributes required to create a valid
   # Budget. As you add validations to Budget, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+  let(:valid_attributes) do
+    skip('Add a hash of attributes valid for your model')
+  end
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+  let(:invalid_attributes) do
+    skip('Add a hash of attributes invalid for your model')
+  end
 
-  describe "GET /index" do
-    it "renders a successful response" do
+  describe 'GET /index' do
+    it 'renders a successful response' do
       Budget.create! valid_attributes
       get budgets_url
       expect(response).to be_successful
     end
   end
 
-  describe "GET /show" do
-    it "renders a successful response" do
+  describe 'GET /show' do
+    it 'renders a successful response' do
       budget = Budget.create! valid_attributes
       get budget_url(budget)
       expect(response).to be_successful
     end
   end
 
-  describe "GET /new" do
-    it "renders a successful response" do
+  describe 'GET /new' do
+    it 'renders a successful response' do
       get new_budget_url
       expect(response).to be_successful
     end
   end
 
-  describe "GET /edit" do
-    it "renders a successful response" do
+  describe 'GET /edit' do
+    it 'renders a successful response' do
       budget = Budget.create! valid_attributes
       get edit_budget_url(budget)
       expect(response).to be_successful
     end
   end
 
-  describe "POST /create" do
-    context "with valid parameters" do
-      it "creates a new Budget" do
-        expect {
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'creates a new Budget' do
+        expect do
           post budgets_url, params: { budget: valid_attributes }
-        }.to change(Budget, :count).by(1)
+        end.to change(Budget, :count).by(1)
       end
 
-      it "redirects to the created budget" do
+      it 'redirects to the created budget' do
         post budgets_url, params: { budget: valid_attributes }
         expect(response).to redirect_to(budget_url(Budget.last))
       end
     end
 
-    context "with invalid parameters" do
-      it "does not create a new Budget" do
-        expect {
+    context 'with invalid parameters' do
+      it 'does not create a new Budget' do
+        expect do
           post budgets_url, params: { budget: invalid_attributes }
-        }.to change(Budget, :count).by(0)
+        end.to change(Budget, :count).by(0)
       end
 
       it "renders a successful response (i.e. to display the 'new' template)" do
@@ -84,20 +83,20 @@ RSpec.describe "/budgets", type: :request do
     end
   end
 
-  describe "PATCH /update" do
-    context "with valid parameters" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+  describe 'PATCH /update' do
+    context 'with valid parameters' do
+      let(:new_attributes) do
+        skip('Add a hash of attributes valid for your model')
+      end
 
-      it "updates the requested budget" do
+      it 'updates the requested budget' do
         budget = Budget.create! valid_attributes
         patch budget_url(budget), params: { budget: new_attributes }
         budget.reload
-        skip("Add assertions for updated state")
+        skip('Add assertions for updated state')
       end
 
-      it "redirects to the budget" do
+      it 'redirects to the budget' do
         budget = Budget.create! valid_attributes
         patch budget_url(budget), params: { budget: new_attributes }
         budget.reload
@@ -105,7 +104,7 @@ RSpec.describe "/budgets", type: :request do
       end
     end
 
-    context "with invalid parameters" do
+    context 'with invalid parameters' do
       it "renders a successful response (i.e. to display the 'edit' template)" do
         budget = Budget.create! valid_attributes
         patch budget_url(budget), params: { budget: invalid_attributes }
@@ -114,15 +113,15 @@ RSpec.describe "/budgets", type: :request do
     end
   end
 
-  describe "DELETE /destroy" do
-    it "destroys the requested budget" do
+  describe 'DELETE /destroy' do
+    it 'destroys the requested budget' do
       budget = Budget.create! valid_attributes
-      expect {
+      expect do
         delete budget_url(budget)
-      }.to change(Budget, :count).by(-1)
+      end.to change(Budget, :count).by(-1)
     end
 
-    it "redirects to the budgets list" do
+    it 'redirects to the budgets list' do
       budget = Budget.create! valid_attributes
       delete budget_url(budget)
       expect(response).to redirect_to(budgets_url)
