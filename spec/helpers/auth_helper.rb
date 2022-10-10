5d187e838d6884e64261d379533558a89235d4cb
Gabriel <gopxfs@hotmail.com>
2022-10-10 19:26:36 -0300

    Add helper method to testing

---
+++ spec/helpers/auth_helper.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+module AuthHelper
+  def login_user
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    user = FactoryGirl.create(:user)
+    sign_in user
+  end
+end
