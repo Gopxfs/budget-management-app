27602067fdb5c478468616865abd8dddb83d45f1
Gabriel <gopxfs@hotmail.com>
2022-10-10 17:32:31 -0300

    Add money properties to amount in budget migration

--- db/migrate/20221010193249_create_budget.rb
+++ db/migrate/20221010193249_create_budget.rb
@@ -2,7 +2,7 @@ class CreateBudget < ActiveRecord::Migration[7.0]
   def change
     create_table :budgets do |t|
       t.string :name, default: 'Budget name'
-      t.decimal :amount, default: 0
+      t.decimal :amount, default: 0, precision: 15, scale: 2
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
