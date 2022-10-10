631ff1429dbc3d3d3402cb50b97937adbc4a342a
Gabriel <gopxfs@hotmail.com>
2022-10-10 17:35:50 -0300

    Add category icon placeholder

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
   has_and_belongs_to_many :budgets
   belongs_to :user
+
+  validates 
 end
--- db/migrate/20221010193339_create_group.rb
+++ db/migrate/20221010193339_create_group.rb
@@ -2,7 +2,7 @@ class CreateGroup < ActiveRecord::Migration[7.0]
   def change
     create_table :groups do |t|
       t.string :name, default: 'Group name'
-      t.string :icon
+      t.string :icon, default: "app/assets/images/category-icon-placeholder.png"
 
       t.timestamps
     end
