4e89bbb38c5ba23296c9efe9c355453a4e2bd7c0
Gabriel <gopxfs@hotmail.com>
2022-10-10 20:21:17 -0300

    Add test to categories page

---
+++ spec/requests/groups_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+require './spec/helpers/auth_helper'
+
+RSpec.configure do |c|
+  c.include AuthHelper
+end
+
+RSpec.describe 'Groups Controller', type: :request do
+  subject do
+    User.new(
+      name: 'Gabriel',
+      email: 'example@email.com',
+      password: '123123',
+    )
+  end
+
+  context "GET #index" do
+    it "renders the :index view" do 
+      sign_in(subject)
+      get '/groups'
+
+      expect(response).to be_successful
+
+      expect(response).to render_template(:index)
+    end
+  end
+end
