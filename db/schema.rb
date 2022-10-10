d8c17d7d16f83981c07011f28ac95aa51e86811a
Gabriel <gopxfs@hotmail.com>
2022-10-10 18:20:22 -0300

    Add application_controller parameters

--- db/schema.rb
+++ db/schema.rb
@@ -35,8 +35,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_200001) do
   create_table "groups", force: :cascade do |t|
     t.string "name", default: "Group name"
     t.string "icon", default: "app/assets/images/category-icon-placeholder.png"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -53,4 +55,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_200001) do
   end
 
   add_foreign_key "budgets", "users"
+  add_foreign_key "groups", "users"
 end
