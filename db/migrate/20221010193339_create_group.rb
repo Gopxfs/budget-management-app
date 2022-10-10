73c86f3764259f19b3154702a4b85a2aea1935e4
Gabriel <gopxfs@hotmail.com>
2022-10-10 18:15:26 -0300

    Add reference to user table

--- db/migrate/20221010193339_create_group.rb
+++ db/migrate/20221010193339_create_group.rb
@@ -3,6 +3,7 @@ class CreateGroup < ActiveRecord::Migration[7.0]
     create_table :groups do |t|
       t.string :name, default: 'Group name'
       t.string :icon, default: "app/assets/images/category-icon-placeholder.png"
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
