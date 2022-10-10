95c4c9df46644ec3cfa8d95570851dfb150c027c
Gabriel <gopxfs@hotmail.com>
2022-10-10 19:10:01 -0300

    Add rails controller testing

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Users Controller', type: :request do
   describe 'GET #index' do
     it 'renders the Users page' do
-      get '/users'
+      get '/users/sign_in'
 
       expect(response).to be_successful
 
-      expect(response).to render_temaplte(:index)
+      expect(response).to render_template(:index)
     end
   end
 end
