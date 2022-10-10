169e1c96ee8e44850a9f35d6d64bb92ef746b478
Gabriel <gopxfs@hotmail.com>
2022-10-10 17:31:59 -0300

    Add validation to budget properties

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,4 +1,7 @@
 class Budget < ApplicationRecord
   has_and_belongs_to_many :groups
   belongs_to :author, class_name: 'User', foreign_key: :user_id
+
+  validates :name, presence: true, length: { minimum: 1, maximum: 40 }
+  validates :amount, numericality: { greater_than_or_equal_to: 0 }
 end
