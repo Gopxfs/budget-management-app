f00bbf10971a09ca7038f137a7239dfde05e27c4
Gabriel <gopxfs@hotmail.com>
2022-10-11 14:48:06 -0300

    Change budgets route to transactions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :groups, :path => 'categories'
-  resources :budgets
+  resources :budgets, :path => 'transactions'
   resources :users
   resources :pages, only: %i[splash]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
