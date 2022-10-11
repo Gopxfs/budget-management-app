a23c150dc4faaa659b8c3ce8b0dbb23e10ed02a3
Gabriel <gopxfs@hotmail.com>
2022-10-11 15:30:37 -0300

    Fix typos

--- app/models/group.rb
+++ app/models/group.rb
@@ -7,7 +7,7 @@ class Group < ApplicationRecord
   def total_price
     sum = 0
     budgets.includes([:budgets_groups]).each do |transaction|
-      sum += amount
+      sum += transaction
     end
     sum
   end
