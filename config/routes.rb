c29ca6720f53116ed12a549947dbe262c5538fac
Gabriel <gopxfs@hotmail.com>
2022-10-11 14:47:49 -0300

    Change groups route to categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :groups
+  resources :groups, :path => 'categories'
   resources :budgets
   resources :users
   resources :pages, only: %i[splash]
