c0e631c440c49d25de28ab66e6b14a89f216223f
Gabriel <gopxfs@hotmail.com>
2022-10-11 16:32:50 -0300

    Change default value for transaction name

--- db/migrate/20221010193249_create_budget.rb
+++ db/migrate/20221010193249_create_budget.rb
@@ -1,7 +1,7 @@
 class CreateBudget < ActiveRecord::Migration[7.0]
   def change
     create_table :budgets do |t|
-      t.string :name, default: 'Budget name'
+      t.string :name, default: 'Transaction name'
       t.decimal :amount, default: 0, precision: 15, scale: 2
       t.references :user, null: false, foreign_key: true
 
