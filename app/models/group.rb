d9b7eb6ad73cec2256cab6c8dcc6709d299a00b7
Gabriel <gopxfs@hotmail.com>
2022-10-12 16:16:28 -0300

    Update minimum category name length

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ApplicationRecord
   has_and_belongs_to_many :budgets
   belongs_to :user
 
-  validates :name, presence: true, length: { minimum: 1, maximum: 40 }
+  validates :name, presence: true, length: { minimum: 0, maximum: 40 }
 
   def total_price
     sum = 0
