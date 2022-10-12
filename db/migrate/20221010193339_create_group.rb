e4bcf30f336771522fcc2387b1b49b2aa8718ec0
Gabriel <gopxfs@hotmail.com>
2022-10-12 15:08:11 -0300

    Change default category name

--- db/migrate/20221010193339_create_group.rb
+++ db/migrate/20221010193339_create_group.rb
@@ -1,7 +1,7 @@
 class CreateGroup < ActiveRecord::Migration[7.0]
   def change
     create_table :groups do |t|
-      t.string :name, default: 'Group name'
+      t.string :name, default: 'Category name'
       t.string :icon, default: "https://cdn-icons-png.flaticon.com/512/94/94699.png"
       t.references :user, null: false, foreign_key: true
 
