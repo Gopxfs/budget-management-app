fdc6abad59a3e92c7f302c35cf643de0bb08ea2a
Gabriel <gopxfs@hotmail.com>
2022-10-12 13:38:33 -0300

    Change default image to URL

--- db/migrate/20221010193339_create_group.rb
+++ db/migrate/20221010193339_create_group.rb
@@ -2,7 +2,7 @@ class CreateGroup < ActiveRecord::Migration[7.0]
   def change
     create_table :groups do |t|
       t.string :name, default: 'Group name'
-      t.string :icon, default: "category-icon-placeholder"
+      t.string :icon, default: "https://cdn-icons-png.flaticon.com/512/94/94699.png"
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
